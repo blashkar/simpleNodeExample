efd0a8e7278d4c2c98ef59e24098b68368cb684d
blashkar <blashkar@ualberta.ca>
2020-09-25 19:51:51 +0000

    1. Simple Node Example

---
+++ index.js
@@ -0,0 +1,28 @@
+var myRect = {
+    area : (x,y) => (x*y),
+    perimeter: (x,y) => (2*(x+y))
+};
+
+function calRect(l,w) {
+    console.log("**** Calculating area & perimeter of rectanglr for length: " + l + " and width: " + w +" ****");
+
+    if (l<=0 || w<=0) {
+        console.log("Error: The diameter should be greater than zero");
+        console.log("\n");
+    } else {
+        console.log("Length: " + l + " Width: "+ w +" The area of rectangle is: " + myRect.area(l,w));
+        console.log("Length: " + l + " Width: "+ w +" The perimeter of rectangle is: " + myRect.perimeter(l,w));
+        console.log("\n");
+    }
+    
+}
+
+calRect(2,3);
+calRect(3,5);
+calRect(0,5);
+calRect(6,-1);
+
+
+
+
+
